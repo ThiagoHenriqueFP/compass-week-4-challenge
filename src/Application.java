b9e55806c4ed32d0d1e12122e63a04eacdb0a183
Thiago Henrique <thiagohp070@gmail.com>
2023-06-27 09:29:47 -0300

    fix: surround objects declarations  with try block

--- src/Application.java
+++ src/Application.java
@@ -6,78 +6,82 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<Vehicle> myVehicles = new ArrayList<>();
+        try {
+            Vehicle hondaCivic = new Car(
+                    "Honda Civic",
+                    "V-Tech 2.2",
+                    true,
+                    5,
+                    4,
+                    false,
+                    500,
+                    true,
+                    true,
+                    2015
+            );
 
-        Vehicle hondaCivic = new Car(
-                "Honda Civic",
-                "V-Tech 2.2",
-                true,
-                5,
-                4,
-                false,
-                500,
-                true,
-                true,
-                2015
-        );
+            Vehicle fusca = new Car(
+                    "Fusca 1600",
+                    "Boxer 1.6",
+                    true,
+                    4,
+                    2,
+                    false,
+                    400,
+                    true,
+                    false,
+                    1965
+            );
 
-        Vehicle fusca = new Car(
-                "Fusca 1600",
-                "Boxer 1.6",
-                true,
-                4,
-                2,
-                false,
-                400,
-                true,
-                false,
-                1965
-        );
+            Vehicle yamahaFazer = new Motocicle(
+                    "Fazer",
+                    "yz 250cc",
+                    2,
+                    false,
+                    200,
+                    true,
+                    2020
+            );
 
-        Vehicle yamahaFazer = new Motocicle(
-                "Fazer",
-                "yz 250cc",
-                2,
-                false,
-                200,
-                true,
-                2020
-        );
+            Vehicle bike = new Bike(
+                    "Absolute nero 4",
+                    2,
+                    true,
+                    200,
+                    true,
+                    true,
+                    2023
+            );
 
-        Vehicle bike = new Bike(
-                "Absolute nero 4",
-                2,
-                true,
-                200,
-                true,
-                true,
-                2023
-        );
+            Vehicle truck = new Truck(
+                    "Astralis",
+                    "Cummins 8.4",
+                    true,
+                    2,
+                    2,
+                    true,
+                    10000,
+                    4,
+                    2001
+            );
 
-        Vehicle truck = new Truck(
-                "Astralis",
-                "Cummins 8.4",
-                true,
-                2,
-                2,
-                true,
-                10000,
-                4,
-                2001
-        );
 
-        myVehicles.add(hondaCivic);
-        myVehicles.add(fusca);
-        myVehicles.add(yamahaFazer);
-        myVehicles.add(bike);
-        myVehicles.add(truck);
+            myVehicles.add(hondaCivic);
+            myVehicles.add(fusca);
+            myVehicles.add(yamahaFazer);
+            myVehicles.add(bike);
+            myVehicles.add(truck);
 
-        myVehicles.forEach(
-                it -> {
-                    System.out.println("Vehicle " + it.getName());
-                    System.out.println("Vehicle " + it.getYear());
-                    System.out.println("------------------------");
-                }
-        );
+            myVehicles.forEach(
+                    it -> {
+                        System.out.println("Vehicle " + it.getName());
+                        System.out.println("Vehicle " + it.getYear());
+                        System.out.println("------------------------");
+                    }
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
